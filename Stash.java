e2f191562018ee4cfe598ccc6b20419ab7a65c71
SelvaRMmalar <srmalar1996@gmail.com>
2020-09-25 05:12:30 +0100

    Stash file branch added

--- Stash.java
+++ Stash.java
@@ -1,3 +1,33 @@
 public class Stash
 {
+	public String getStashId()
+        {
+        }
+         public String getStashName()
+        {
+        }
+
+         public String getStashMessage()
+        {
+        }
+
+        public void setStashId(String stashId)
+        {
+		}
+        public void setStashName(String stashName)
+        {
+		}
+        public void setStashMessage(String stashMessage)
+        {
+        }
+        public void addStash(String stashId)
+        {
+		}
+        public void removeStash(String stashId)
+        {
+        }
+        public void clearAllStash(String stashId)
+        {
+		}
+
 }
